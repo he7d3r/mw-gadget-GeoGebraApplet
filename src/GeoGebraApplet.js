6366e1924b3715c36fef5a7c7d334f4e2ffd6fbb
Helder <he7d3r@gmail.com>
2014-09-20 11:22:28 -0300

    Remove tracking link (only useful on wiki)

--- src/GeoGebraApplet.js
+++ src/GeoGebraApplet.js
@@ -2,7 +2,6 @@
  * Testing GeoGebra applets
  * @author: Helder (https://github.com/he7d3r)
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
- * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/GeoGebraApplet.js]] ([[File:User:Helder.wiki/Tools/GeoGebraApplet.js]])
  */
 /*jslint browser: true, white: true*/
 /*global jQuery, mediaWiki */
